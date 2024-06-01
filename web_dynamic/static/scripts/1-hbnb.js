32b24ceefbdd3b08cd946b3bd01a9d59dc4bed59
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-06-01 03:22:55 +0300

    Ah

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,12 @@
-window.addEventListener('load', function () {
-  // task 2
-  const amenityIds = {};
-  $('input[type=checkbox]').change(function () {
-    if ($(this).prop('checked')) {
-      amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
-    } else if (!$(this).prop('checked')) {
-      delete amenityIds[$(this).attr('data-id')];
-    }
-    if (Object.keys(amenityIds).length === 0) {
-      $('div.amenities h4').html('&nbsp');
-    } else {
-      $('div.amenities h4').text(Object.values(amenityIds).join(', '));
-    }
-  });
-});
+$('document').ready(() => {
+	const amenities = {};
+	$('.amenities .popover input').change(function () {
+		if ($(this).is(':checked')) {
+			amenities[$(this).attr('data-name')] = $(this).attr('data-id');
+		} else if ($(this).is(':not(:checked)')) {
+			delete amenities[$(this).attr('data-name')];
+		}
+		const names = Object.keys(amenities);
+		$('.amenities h4').text(names.sort().join(', '));
+	});
+})
